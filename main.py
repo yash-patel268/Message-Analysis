3564c743d78790575fef541ce729c3b671ebb71b
yash-patel268 <yashp2126@gmail.com>
2023-03-01 18:36:08 -0500

    User can now input message content

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ input_checker = 0
 input_type = ""
 user_email = ""
 user_text = ""
+user_message_content = ""
 
 while input_checker == 0:
     print("Write 'text' or 'email'.")
     input_type = input("Enter the message type: ")
     
     if input_type == 'text' or input_type == 'email':
-        print("The message type selected was: " + input_type + "\n")
+        print("The message type selected is: " + input_type + "\n")
         input_checker = 1
     else:
         print("Wrong input was entered please try again.\n")
@@ -28,7 +29,7 @@ if input_type == 'email':
         user_email = input("Enter the email: ")
         
         if "@" in user_email and "." in user_email:
-            print("The email entered was: " + user_email + "\n")
+            print("The email entered is: " + user_email + "\n")
             input_checker = 1
         else:
             print("Your input is not a proper email.\n")
@@ -44,11 +45,29 @@ if input_type == 'text':
         pattern = r"\d{3}-\d{3}-\d{4}"  
 
         if re.match(pattern, user_text):
-            print("The phone number entered was: " + user_text + "\n")
+            print("The phone number entered is: " + user_text + "\n")
             input_checker = 1
         else:
             print("Invalid phone number\n")
             
         if input_checker == 1:
+            input_checker = 0
+            break
+        
+while input_checker == 0:
+    print("Write or copy/paste the message content.")
+    user_message_content = input("Enter message now: ")
+    print("The message entered is: " + user_message_content)
+    
+    print("Is the message correct enter 'y' or 'n' ")
+    verified_message = input("Enter your decision: ")
+    
+    if verified_message == 'y':
+        print("You have verified the message content.\n")
+        input_checker = 1
+    else:
+        print("Write the message again.\n")
+        
+    if input_checker == 1:
             input_checker = 0
             break
