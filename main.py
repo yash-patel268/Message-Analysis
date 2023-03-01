15fee1eeb735e7ee766d8166b7b4e597ada3fe88
yash-patel268 <yashp2126@gmail.com>
2023-03-01 16:10:09 -0500

    User can now enter message type

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello user, this is the message analysis program which will be used to find phisihing within emails and text messages")
+print("Hello user, this is the message analysis program which will be used to find phisihing within emails and text messages.\n")
+print("For this instance of this application which type of message will be analyzed, text message or emai?\n")
+
+check_message_type = 0
+
+while check_message_type == 0:
+    print("Write 'text' or 'email'.")
+    input_type = input("Enter the message type: ")
+    
+    if input_type == 'text' or input_type == 'email':
+        print("The message type selected was: " + input_type + "\n")
+        check_message_type = 1
+    else:
+        print("Wrong input was entered please try again.\n")
+    
+    if check_message_type == 1:
+        break
