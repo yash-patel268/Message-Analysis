a1f4247ba37c752d4d37bf24a2faa04303d0f981
yash-patel268 <yashp2126@gmail.com>
2023-03-20 17:44:54 -0400

    Added documentation

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassificatio
 import os
 import json
 
+# Initializing the EmoRoBERTa library, code provided by creator, allows us to create a pipeline to feed words to be analyzed
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
 
+# Open email data set, read contents, lower case the strings, assign to common_emails array
 with open("email_domains.json", "r") as f:
     common_emails = json.load(f)
 common_emails = [word.lower() for word in common_emails]
-    
+
+# Open area code data set, read contents, assign to canada_phonenumbers array
 with open("phonenumber_area_codes.json", "r") as f:
     canada_phonenumbers = json.load(f)
     
+# Open phishing word data set, read contents, lower case strings, assign to common_phishing_words array
 with open("phishing_words.json", "r") as f:
     common_phishing_words = json.load(f)
 common_phishing_words = [word.lower() for word in common_phishing_words]
     
+# Initializing overall emotions arrays which are based of results of EmoRoBERTa analysis
 happy_words = ["admiration", "amusement", "approval", "caring", "desire", "excitement", "gratitude", "joy", "love", "optimism", "pride", "relief"]
 depression_words = ["grief", "remorse", "sadness"]
 anger_words = ["anger", "annoyance", "dissapointment", "dissaproval", "digust"]
@@ -28,22 +33,38 @@ os.system('cls')
 print("Hello user, this is the message analysis program which will be used to find phisihing within emails and text messages.\n")
 print("For this instance of this application which type of message will be analyzed, text message or email?\n")
 
+# Initialize var to check user input, will be reused throughout code
 input_checker = 0
+# Initialize var to hold user message type
 input_type = ""
+# Initialize var to hold subject line if user chooses email
 user_email_subject_line = ""
+# Initialize var to hold number of phishing words found in subject line
 subject_line_score = 0
+# Initialize var to hold total number of words in subject line
 subject_line_words = 0
+# Initialize var to hold email  if user chooses email
 user_email = ""
+# Initialize var to decision on whether email entered if they did has common domain
 common_domain = False
+# Initialize var to hold phone number  if user chooses text
 user_text = ""
+# Initialize var to decision on whether phone number entered has common area code
 common_area_code = False
+# Initialize var to hold main message regardless of email or text
 user_message_content = ""
+# Initialize var to hold number of phishing words found in message content
 message_content_score = 0
+# Initialize var to hold total number of words in message content
 message_content_words = 0
+# Initialize var to hold emotion of subject line if they choose email
 subject_line_emotion = ""
+# Initialize var to hold emotion of main message
 message_emotion = ""
+# Initialize var to hold overall phishing score
 total_phishing_score = 0
 
+# Loop to get user input of what type of message they would like to analyze
 while input_checker == 0:
     print("Write 'text' or 'email'.")
     input_type = input("Enter the message type: ")
@@ -58,6 +79,7 @@ while input_checker == 0:
         input_checker = 0
         break
 
+# Loop to obtain proper email if the user chooses email as the focus of the application
 if input_type == 'email':
     while input_checker == 0:
         print("Write the email of sender.")
@@ -72,6 +94,7 @@ if input_type == 'email':
         if input_checker == 1:
             input_checker = 0
             break
+# Loop to obtain proper phone number if the user chooses text as the focus of the application
 else:
     while input_checker == 0:
         print("Write the phone number of sender in XXX-XXX-XXXX format")
@@ -88,6 +111,7 @@ else:
             input_checker = 0
             break
       
+# Loop to get subject line that is entered and is confirmed by user if email is focus
 if input_type == 'email':
     while input_checker == 0:
         print("Write the emails subject line from sender.")
@@ -107,7 +131,8 @@ if input_type == 'email':
                 input_checker = 0
                 verified_message = ""
                 break 
-        
+
+# Loop to get main messaged that is entered and is confirmed by user regardless of text or email is focus    
 while input_checker == 0:
     print("Write or copy/paste the message content.")
     user_message_content = input("Enter message now: ")
@@ -127,11 +152,13 @@ while input_checker == 0:
             verified_message = ""
             break  
         
+# Determine whether email is within common email dataset, if not increment overall phishing score, if email was choosen as focus
 if input_type == 'email':
     for item in common_emails:
         if item in user_email:
             input_checker = 1
     
+    # Start of report for email
     if input_checker == 1:
         print("Entered email has common domain.")
         common_domain = True
@@ -139,11 +166,13 @@ if input_type == 'email':
         print("Entered email doesn't have common domain.")
         common_domain = False
         total_phishing_score += 1
+# Determine whether area code of phone number is within canada area code dataset, if not increment overall phishing score, if text was choosen as focus        
 else:
     for item in canada_phonenumbers:
         if item in user_text[:3]:
             input_checker = 1
-            
+    
+    # Start of report for text
     if input_checker == 1:
         print("Entered phone number is part of the area codes in Canada.")
         common_domain = True
@@ -152,6 +181,11 @@ else:
         common_domain = False
         total_phishing_score += 1
         
+# Determine whether phishing is present within subject line, done by determining emotion and number of phishing words,
+# the phishing words is then compared to overall words and if its less than 30% of subject line its considered safe,
+# else overall phishing score is increment
+# emotion is dependent on whether if theres phishing words are present, else it would be a false reading,
+# if neutral, anxiety, or anger increment since it suspicious phrase tend to lay within those emotions. 
 if input_type == "email":
     for item in common_phishing_words:
         if item in user_email_subject_line.lower():
@@ -177,7 +211,8 @@ if input_type == "email":
         subject_line_emotion = "neutral"
         if subject_line_score != 0:
             total_phishing_score += 1
-        
+    
+    # Inclusion of subject for report if email is chosen as focus 
     print("Emotion detected in subject line is: " + subject_line_emotion)
     
     print("Number of phishing words used in the subject line is: " + str(subject_line_score))
@@ -193,6 +228,11 @@ if input_type == "email":
         print("Phishing attempt was detected in the subject line.")
         total_phishing_score += 1
 
+# Determine whether phishing is present within main message, done by determining emotion and number of phishing words,
+# the phishing words is then compared to overall words and if its less than 20% of main message and it has less than 5 phishing words
+# its considered safe, else overall phishing score is increment
+# emotion is dependent on whether if theres phishing words are present, else it would be a false reading,
+# if neutral, anxiety, or anger increment since it suspicious phrase tend to lay within those emotions. 
 for item in common_phishing_words:
     if item in user_message_content.lower():
         message_content_score += 1
@@ -218,6 +258,7 @@ else:
     if message_content_score != 0:
         total_phishing_score += 1
     
+# Rest of report is printed regardless of focus
 print("Emotion detected in message content is: " + message_emotion)
     
 print("Number of phishing words used in the message content is: " + str(message_content_score))
@@ -233,9 +274,14 @@ else:
     total_phishing_score += 1
     print("Phishing attempt was detected in the message content.")
     
+# Overall phishing score for email determined by 5 different aspects:
+# email domain, number of phishing words in subject line, emotion in subject line,
+# number of phishing words in main message, and emotion in main message.
 if input_type == "email":
     total_phishing_score = round((total_phishing_score/5)*100, 2)
     print("The phishing score is: " + str(total_phishing_score) + "%")
+# Overall phishing score for text determined by 3 different aspects:
+# phone number area code,number of phishing words in main message, and emotion in main message.
 else:
     total_phishing_score = round((total_phishing_score/3)*100, 2)
     print("The phishing score is: " + str(total_phishing_score) + "%")
